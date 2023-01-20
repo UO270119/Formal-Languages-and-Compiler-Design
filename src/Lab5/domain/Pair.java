ed6ba11b92029997f7a00f0b67c9ca0ff1194de0
UO270119 <78871046+UO270119@users.noreply.github.com>
2023-01-20 17:54:12 +0200

    All labs delivered

---
+++ src/Lab5/domain/Pair.java
@@ -0,0 +1,20 @@
+package Lab5.domain;
+
+public class Pair<T,Q> {
+
+    private T key;
+    private Q value;
+
+    public Pair(T key,Q value) {
+        this.key=key;
+        this.value=value;
+    }
+
+    public T getKey() {
+        return key;
+    }
+
+    public Q getValue() {
+        return value;
+    }
+}
